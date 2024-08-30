0c70861b6a4596abb8c9ed9a57d62b254dbe42df
Masega360 <tomicaporaso@gmail.com>
2024-08-30 11:17:31 -0300

    Subo tp4 v1

--- src/main/java/com/template/Elevator.java
+++ src/main/java/com/template/Elevator.java
@@ -1,6 +1,46 @@
 package com.template;
 
 public class Elevator {
+    int floor;
+    int passenger;
+    int maxFloor;
+    int minFloor;
+    int maxPassenger ;
+
+    public Elevator(int minFloor, int maxFloor, int maxPassenger) {
+        this.minFloor = minFloor;
+        this.maxFloor = maxFloor;
+        this.maxPassenger = maxPassenger;
+    }
+    public void moveToFloor(int floorNew) {
+        if (floorNew <= maxFloor && floorNew >= minFloor) {
+            this.floor = floorNew;
+        }
+    }
+
+    public int getCurrentFloor() {
+        return this.floor;
+    }
+
+    public void addPassenger() {
+        int passengerNewAmount = this.passenger + 1;
+        if (passengerNewAmount <= maxPassenger) {
+            this.passenger = passengerNewAmount;
+        }
+    }
+
+    public int getPassengerCount() {
+        return this.passenger;
+    }
+
+    public void removePassenger() {
+        int passengerNRemoveAmount = this.passenger - 1;
+        if (passengerNRemoveAmount > 0) {
+            this.passenger = passengerNRemoveAmount;
+        }
+
+
+    }
 
 
 
